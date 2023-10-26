954dc04c78e5ada21c4d26645ec1b7aa10b8ad37
anooshiravan-G1 <68273221+anooshiravan-G1@users.noreply.github.com>
2023-10-26 20:58:08 +0330

    done for now - need some updates

--- controller/controller.py
+++ controller/controller.py
@@ -10,9 +10,12 @@ class InstaLoaderController:
 
     def download_all_posts(self, username):
         self.model.download_all_posts(username)
-    
-    def download_stories(self, username):
-        self.model.download_stories(username)
 
     def download_bio(self, username):
         return self.model.download_bio(username)
+    
+    def save_followings_to_file(self, username):
+        followings = self.model.get_followings(username)
+        with open('followings.txt', 'w') as f:
+            for following in followings:
+                f.write(following + '\n')
--- views/view.py
+++ views/view.py
@@ -1,8 +1,39 @@
 # view.py
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QInputDialog, QMessageBox, QLineEdit
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QInputDialog, QMessageBox, QLineEdit, QDialog, QLabel
 
 from controller.controller import InstaLoaderController
+
+
+class CustomInputDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Write your ID here")
+        self.setStyleSheet("background-color: white; color: black;")
+
+        layout = QVBoxLayout(self)
+
+        self.input_field = QLineEdit(self)
+        layout.addWidget(self.input_field)
+
+        ok_button = QPushButton("OK", self)
+        ok_button.clicked.connect(self.accept)
+        layout.addWidget(ok_button)
+
+    def getText(self):
+        return self.input_field.text()
+
+
+def display_message(message_text, messageTitle):
+    msgBox = QMessageBox()
+    msgBox.setStyleSheet("QMessageBox {background-color: white;}")
+    msgBox.setIcon(QMessageBox.Information)
+    msgBox.setText(message_text)
+    msgBox.setWindowTitle(messageTitle)
+    msgBox.exec_()
+
+
 
 class InstaLoaderView(QWidget):
     def __init__(self):
@@ -13,42 +44,47 @@ class InstaLoaderView(QWidget):
 
         #window Visuals
         self.setWindowTitle("InstaLoader")
-        self.setGeometry(100, 100, 800, 600)
+        self.setGeometry(100, 100, 400, 200)
         self.setStyleSheet("background-color: #23242a;")
         self.setWindowTitle("InstaLoader")
+
+        # Layout
         self.layout = QVBoxLayout()
+
         
-        
+        # Buttons
         self.download_pic_button = QPushButton("Download Profile Picture", self)
         self.download_posts_button = QPushButton("Download All Posts", self)
-        self.download_stories_button = QPushButton("Download Stories", self)
         self.download_bio_button = QPushButton("Download Bio", self)
+        self.save_followings_button = QPushButton("Save Followings", self)
         self.exit_button = QPushButton("Exit", self)
         
+        # Add buttons to layout
         self.layout.addWidget(self.download_pic_button)
         self.layout.addWidget(self.download_posts_button)
-        self.layout.addWidget(self.download_stories_button)
         self.layout.addWidget(self.download_bio_button)
+        self.layout.addWidget(self.save_followings_button)
         self.layout.addWidget(self.exit_button)
-        
+
 
 
         # Set button width to capture half of the window
-        button_width = self.width() / 2
-        button_height = self.height() / 12
+        button_width = self.width() 
+        button_height = self.height() / 4
         self.download_pic_button.setFixedSize(button_width, button_height)
         self.download_posts_button.setFixedSize(button_width, button_height)
-        self.download_stories_button.setFixedSize(button_width, button_height)
         self.download_bio_button.setFixedSize(button_width, button_height)
+        self.save_followings_button.setFixedSize(button_width, button_height)
         self.exit_button.setFixedSize(button_width, button_height)
 
         # Align buttons to the left
-        self.layout.addWidget(self.download_pic_button, alignment=Qt.AlignLeft)
-        self.layout.addWidget(self.download_posts_button, alignment=Qt.AlignLeft)
-        self.layout.addWidget(self.download_stories_button, alignment=Qt.AlignLeft)
-        self.layout.addWidget(self.download_bio_button, alignment=Qt.AlignLeft)
-        self.layout.addWidget(self.exit_button, alignment=Qt.AlignLeft)
+        self.layout.addWidget(self.download_pic_button, alignment=Qt.AlignHCenter)
+        self.layout.addWidget(self.download_posts_button, alignment=Qt.AlignHCenter)
+        self.layout.addWidget(self.download_bio_button, alignment=Qt.AlignHCenter)
+        self.layout.addWidget(self.save_followings_button, alignment=Qt.AlignHCenter)
+        self.layout.addWidget(self.exit_button, alignment=Qt.AlignCenter)
 
+        # Set layout
         self.setLayout(self.layout)
 
 
@@ -58,9 +94,9 @@ class InstaLoaderView(QWidget):
                                                "QPushButton:hover { background-color: grey; }" % radius )
         self.download_posts_button.setStyleSheet("QPushButton { background-color: white; color: black; border: 2px solid white; border-radius: %dpx; font-size: 14px; }"
                                                  "QPushButton:hover { background-color: grey; }" % radius )
-        self.download_stories_button.setStyleSheet("QPushButton { background-color: white; color: black; border: 2px solid white; border-radius: %dpx; font-size: 14px; }"
-                                               "QPushButton:hover { background-color: grey; }" % radius )
         self.download_bio_button.setStyleSheet("QPushButton { background-color: white; color: black; border: 2px solid white; border-radius: %dpx; font-size: 14px; }"
+                                                 "QPushButton:hover { background-color: grey; }" % radius )
+        self.save_followings_button.setStyleSheet("QPushButton { background-color: white; color: black; border: 2px solid white; border-radius: %dpx; font-size: 14px; }"
                                                  "QPushButton:hover { background-color: grey; }" % radius )
         self.exit_button.setStyleSheet("QPushButton { background-color: white; color: black; border: 2px solid white; border-radius: %dpx; font-size: 14px; }"
                                        "QPushButton:hover { background-color: grey; }" % radius )
@@ -68,51 +104,44 @@ class InstaLoaderView(QWidget):
 
         self.download_pic_button.clicked.connect(self.download_profile_pic)
         self.download_posts_button.clicked.connect(self.download_all_posts)
-        self.download_stories_button.clicked.connect(self.download_stories)
         self.download_bio_button.clicked.connect(self.download_bio)
+        self.save_followings_button.clicked.connect(self.save_followings)
         self.exit_button.clicked.connect(self.close)
 
+
+    
     def download_profile_pic(self):
-        style_sheet = (
-            "QLineEdit { background-color: white; color: black; border: 2px solid white; border-radius: 5px; }"
-            "QPushButton { background-color: white; color: black; border: 2px solid white; border-radius: 5px; }"
-        )
-        text.setStyleSheet(style_sheet)
-        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your Instagram ID:', QLineEdit.Normal, '', flags=Qt.CustomizeWindowHint)
-        
-        if ok:
-            self.controller.download_profile_pic(text)
+        custom_dialog = CustomInputDialog(self)
+        if custom_dialog.exec_() == QDialog.Accepted:
+            text = custom_dialog.getText()
+            if text:
+                self.controller.download_profile_pic(text)
+
 
     def download_all_posts(self):
-        style_sheet = (
-            "QLineEdit { background-color: white; color: black; border: 2px solid white; border-radius: 5px; }"
-            "QPushButton { background-color: white; color: black; border: 2px solid white; border-radius: 5px; }"
-        )
-        text.setStyleSheet(style_sheet)
-        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your Instagram ID:', QLineEdit.Normal, '', flags=Qt.CustomizeWindowHint)
-        
-        if ok:
-            self.controller.download_all_posts(text)
-    
-    def download_stories(self):
-        style_sheet = (
-            "QLineEdit { background-color: white; color: black; border: 2px solid white; border-radius: 5px; }"
-            "QPushButton { background-color: white; color: black; border: 2px solid white; border-radius: 5px; }"
-        )
-        text.setStyleSheet(style_sheet)
-        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your Instagram ID:', QLineEdit.Normal, '', flags=Qt.CustomizeWindowHint)
-        
-        if ok:
-            self.controller.download_stories(text)
+
+        custom_dialog = CustomInputDialog(self)
+        if custom_dialog.exec_() == QDialog.Accepted:
+            text = custom_dialog.getText()
+            if text:
+                self.controller.download_all_posts(text)
+
 
     def download_bio(self):
-        style_sheet = (
-            "QLineEdit { background-color: white; color: black; border: 2px solid white; border-radius: 5px; }"
-            "QPushButton { background-color: white; color: black; border: 2px solid white; border-radius: 5px; }"
-        )
-        text.setStyleSheet(style_sheet)
-        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your Instagram ID:', QLineEdit.Normal, '', flags=Qt.CustomizeWindowHint)
-        
-        if ok:
-            bio = self.controller.download_bio(text)
-            QMessageBox.information(self, "Bio", bio)
+
+        custom_dialog = CustomInputDialog(self)
+        if custom_dialog.exec_() == QDialog.Accepted:
+            text = custom_dialog.getText()
+            if text:
+                bio = self.controller.download_bio(text)
+                #QMessageBox.information(self, "Bio", bio)
+                display_message(bio, "Bio")
+
+
+    def save_followings(self):
+      dialog = CustomInputDialog()
+      ok = dialog.exec_()
+      username_story_page = dialog.getText()[0] if ok else None
+
+      if username_story_page:
+        self.controller.save_followings_to_file(username_story_page)
